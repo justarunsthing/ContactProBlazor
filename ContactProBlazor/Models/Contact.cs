a20a51347b014dd179df0f792293c21511e412f7
justarunsthing <justarunsthing@outlook.com>
2026-01-22 08:15:17 +0000

    Updated Contact model

--- ContactProBlazor/Models/Contact.cs
+++ ContactProBlazor/Models/Contact.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel;
+﻿using ContactProBlazor.Data;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -48,5 +48,30 @@ namespace ContactProBlazor.Models
         [Required]
         [Display(Name = "Postcode")]
         public string? PostCode { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        [Phone]
+        [Display(Name = "Phone Number")]
+        public string? PhoneNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTimeOffset Created
+        {
+            get => _created;
+            set => _created = value.ToUniversalTime();
+        }
+
+        [Required]
+        public string? AppUserId { get; set; }
+        public Guid? ImageId { get; set; }
+        public virtual ImageUpload? Image { get; set; }
+
+        // Creates FK relationship to AspNetUsers table
+        public virtual ApplicationUser? AppUser { get; set; }
     }
 }
