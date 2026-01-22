f385e51e7954ed8c50616ce9b2cd8c19f005ad83
justarunsthing <justarunsthing@outlook.com>
2026-01-22 08:18:57 +0000

    Added Category model

---
+++ ContactProBlazor/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using ContactProBlazor.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContactProBlazor.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Category Name")]
+        public string? Name { get; set; }
+
+        [Required]
+        public string? AppUserId { get; set; }
+        public virtual ApplicationUser? AppUser { get; set; }
+        public virtual ICollection<Contact>? Contacts { get; set; } = [];
+    }
+}
