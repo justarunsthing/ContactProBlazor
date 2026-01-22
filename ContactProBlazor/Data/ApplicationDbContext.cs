eb93d290a3b48fe7b774eddac1fccc4d0eb0a804
justarunsthing <justarunsthing@outlook.com>
2026-01-22 08:19:54 +0000

    Added DbSets for contact & category

--- ContactProBlazor/Data/ApplicationDbContext.cs
+++ ContactProBlazor/Data/ApplicationDbContext.cs
@@ -7,5 +7,7 @@ namespace ContactProBlazor.Data
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
         public DbSet<ImageUpload> Images { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
