1642b8277e790abf1392dcca37c224f60e9faf3c
justarunsthing <justarunsthing@outlook.com>
2026-01-23 08:14:42 +0000

    Added method to seed demo user

--- ContactProBlazor/Data/DataUtility.cs
+++ ContactProBlazor/Data/DataUtility.cs
@@ -14,6 +14,51 @@ namespace ContactProBlazor.Data
 
             // Apply any pending migrations
             await dbContextService.Database.MigrateAsync();
+            // Seed a demo user
+            await SeedDemoUserAsync(userManager, config);
+        }
+
+        public static async Task SeedDemoUserAsync(UserManager<ApplicationUser> userManager, IConfiguration config)
+        {
+            try
+            {
+                var demoEmail = config["DemoUserLogin"];
+                var demoPassword = config["DemoUserPassword"];
+
+                if (string.IsNullOrEmpty(demoEmail) || string.IsNullOrEmpty(demoPassword))
+                {
+                    throw new Exception("Demo user login or password not found in configuration, skipping demo user seeding.");
+                }
+
+                ApplicationUser demoUser = new()
+                {
+                    UserName = demoEmail,
+                    Email = demoEmail,
+                    FirstName = "Demo",
+                    LastName = "User",
+                    EmailConfirmed = true
+                };
+
+                var user = await userManager.FindByEmailAsync(demoUser.Email);
+
+                if (user == null)
+                {
+                    var result = await userManager.CreateAsync(demoUser, demoPassword);
+
+                    if (!result.Succeeded)
+                    {
+                        throw new Exception("Failed to create demo user.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("******* ERROR SEEDING DEMO USER *********");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("*****************************************");
+
+                throw;
+            }
         }
     }
 }
