3d16afe24a3b53fce4aeed051ff7aa162425124b
justarunsthing <justarunsthing@outlook.com>
2026-01-23 08:01:32 +0000

    Added DataUtility class

---
+++ ContactProBlazor/Data/DataUtility.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactProBlazor.Data
+{
+    public class DataUtility
+    {
+        // IServiceProvider provides a hook to any services registered
+        public static async Task ManageDataAsync(IServiceProvider serviceProvider)
+        {
+            var dbContextService = serviceProvider.GetRequiredService<ApplicationDbContext>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            var config = serviceProvider.GetRequiredService<IConfiguration>();
+
+            // Apply any pending migrations
+            await dbContextService.Database.MigrateAsync();
+        }
+    }
+}
