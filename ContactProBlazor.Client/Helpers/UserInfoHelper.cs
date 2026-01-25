12c4563e72a289f636be60dfedf02546963a74e0
justarunsthing <justarunsthing@outlook.com>
2026-01-25 13:24:17 +0000

    Added GetUserInfo method

--- ContactProBlazor.Client/Helpers/UserInfoHelper.cs
+++ ContactProBlazor.Client/Helpers/UserInfoHelper.cs
@@ -36,5 +36,28 @@ namespace ContactProBlazor.Client.Helpers
                 return null;
             }
         }
+
+        public static UserInfo? GetUserInfo(AuthenticationState authState)
+        {
+            var userId = authState.User.FindFirst(ClaimTypes.NameIdentifier)!.Value;
+            var email = authState.User.FindFirst(ClaimTypes.Email)!.Value;
+            var firstName = authState.User.FindFirst("FirstName")!.Value;
+            var lastName = authState.User.FindFirst("LastName")!.Value;
+            var profilePictureUrl = authState.User.FindFirst(nameof(UserInfo.ProfilePictureUrl))!.Value;
+
+            if (string.IsNullOrEmpty(userId) || string.IsNullOrEmpty(email) || string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(profilePictureUrl))
+            {
+                return null;
+            }
+
+            return new UserInfo
+            {
+                UserId = userId,
+                Email = email,
+                FirstName = firstName,
+                LastName = lastName,
+                ProfilePictureUrl = profilePictureUrl
+            };
+        }
     }
 }
