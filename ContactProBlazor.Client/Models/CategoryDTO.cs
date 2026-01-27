c577ee1f8f76e9f37b5ff4eed23301a92de24574
justarunsthing <justarunsthing@outlook.com>
2026-01-27 08:15:17 +0000

    Added CategoryDTO

---
+++ ContactProBlazor.Client/Models/CategoryDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactProBlazor.Client.Models
+{
+    public class CategoryDTO
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Category Name")]
+        public string? Name { get; set; }
+        public virtual ICollection<ContactDTO>? Contacts { get; set; } = [];
+    }
+}
