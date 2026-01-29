29e3a6f2feb73909447fb81bc06fd355d781b868
justarunsthing <justarunsthing@outlook.com>
2026-01-29 08:13:32 +0000

    Added skeleton CategoryRepository

---
+++ ContactProBlazor/Services/CategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using ContactProBlazor.Data;
+using ContactProBlazor.Models;
+using ContactProBlazor.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactProBlazor.Services
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        public CategoryRepository(IDbContextFactory<ApplicationDbContext> contextFactory)
+        {
+            
+        }
+
+        public Task<Category> CreateCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
