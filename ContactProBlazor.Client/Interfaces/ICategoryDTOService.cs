3c24ff5243a9a07ceffff2a4b56de46133917352
justarunsthing <justarunsthing@outlook.com>
2026-01-29 08:18:44 +0000

    Added ICategoryDTOService interface

---
+++ ContactProBlazor.Client/Interfaces/ICategoryDTOService.cs
@@ -0,0 +1,9 @@
+﻿using ContactProBlazor.Client.Models;
+
+namespace ContactProBlazor.Client.Interfaces
+{
+    public interface ICategoryDTOService
+    {
+        Task<CategoryDTO> CreateCategoryAsync(CategoryDTO category, string userId);
+    }
+}
