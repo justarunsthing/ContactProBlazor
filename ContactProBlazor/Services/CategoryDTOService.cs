c5e363fc71c18aa09ecfba14a1ddce810f718760
justarunsthing <justarunsthing@outlook.com>
2026-01-29 08:25:45 +0000

    Added CategoryDTOService and implemented ICategoryDTOService

---
+++ ContactProBlazor/Services/CategoryDTOService.cs
@@ -0,0 +1,23 @@
+﻿using ContactProBlazor.Models;
+using ContactProBlazor.Interfaces;
+using ContactProBlazor.Client.Models;
+using ContactProBlazor.Client.Interfaces;
+
+namespace ContactProBlazor.Services
+{
+    public class CategoryDTOService(ICategoryRepository repository) : ICategoryDTOService
+    {
+        public async Task<CategoryDTO> CreateCategoryAsync(CategoryDTO category, string userId)
+        {
+            Category newCategory = new()
+            {
+                AppUserId = userId,
+                Name = category.Name
+            };
+
+            newCategory = await repository.CreateCategoryAsync(newCategory);
+
+            return newCategory.ToDTO();
+        }
+    }
+}
