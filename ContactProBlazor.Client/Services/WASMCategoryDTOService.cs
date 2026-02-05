d6e639d8f96779f4e9742f7cb09571d9db1e35af
justarunsthing <justarunsthing@outlook.com>
2026-02-05 08:37:10 +0000

    Added skeleton WASMCategoryDTOService class

---
+++ ContactProBlazor.Client/Services/WASMCategoryDTOService.cs
@@ -0,0 +1,33 @@
+﻿using ContactProBlazor.Client.Models;
+using ContactProBlazor.Client.Interfaces;
+
+namespace ContactProBlazor.Client.Services
+{
+    public class WASMCategoryDTOService(HttpClient http) : ICategoryDTOService
+    {
+        public Task<CategoryDTO> GetCategoryByIdAsync(int id, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<CategoryDTO>> GetCategoriesAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CategoryDTO> CreateCategoryAsync(CategoryDTO category, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCategoryAsync(CategoryDTO category, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCategoryAsync(int id, string userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
