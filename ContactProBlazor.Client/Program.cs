89cd627b3a5d0319a7ced9972d6d80119256c8fa
justarunsthing <justarunsthing@outlook.com>
2026-02-05 08:42:54 +0000

    Register services

--- ContactProBlazor.Client/Program.cs
+++ ContactProBlazor.Client/Program.cs
@@ -1,3 +1,5 @@
+using ContactProBlazor.Client.Services;
+using ContactProBlazor.Client.Interfaces;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -5,5 +7,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddAuthenticationStateDeserialization();
+
+// Services
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<ICategoryDTOService, WASMCategoryDTOService>();
 
 await builder.Build().RunAsync();
