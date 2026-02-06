802aa8cacb62fa5f9e72020bfbbd652cc75a205a
justarunsthing <justarunsthing@outlook.com>
2026-02-06 08:05:00 +0000

    Implemented GetCategoryAsync method

--- ContactProBlazor/Controllers/CategoriesController.cs
+++ ContactProBlazor/Controllers/CategoriesController.cs
@@ -14,8 +14,24 @@ namespace ContactProBlazor.Controllers
     public class CategoriesController(ICategoryDTOService categoryDTOService, UserManager<ApplicationUser> userManager) : ControllerBase
     {
         // Authorize means userId will never be null
-        // Is an expression as it needs to be evaluated
+        // Is an expression bodied memner as it needs to be evaluated everytime this controller is accessed
         private string _userId => userManager.GetUserId(User)!;
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CategoryDTO>> GetCategoryAsync(int id)
+        {
+            try
+            {
+                CategoryDTO? category = await categoryDTOService.GetCategoryByIdAsync(id, _userId);
+
+                return category == null ? NotFound() : category;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return Problem();
+            }
+        }
 
         [HttpGet]
         public async Task<ActionResult<List<CategoryDTO>>> GetCategoriesAsync()
