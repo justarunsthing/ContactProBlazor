9c7674830b6755b576a85adb3f74babddd096863
justarunsthing <justarunsthing@outlook.com>
2026-02-12 08:04:35 +0000

    Added ContactRepository

---
+++ ContactProBlazor/Services/ContactRepository.cs
@@ -0,0 +1,15 @@
+﻿using ContactProBlazor.Data;
+using ContactProBlazor.Models;
+using ContactProBlazor.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactProBlazor.Services
+{
+    public class ContactRepository(IDbContextFactory<ApplicationDbContext> contextFactory) : IContactRepository
+    {
+        public Task<Contact> CreateContactAsync(Contact contact)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
