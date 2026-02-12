86ab8cd67c1eddc1e2a4d25c43ea24fb8103c802
justarunsthing <justarunsthing@outlook.com>
2026-02-12 08:11:25 +0000

    Added ContactDTOService class

---
+++ ContactProBlazor/Services/ContactDTOService.cs
@@ -0,0 +1,14 @@
+﻿using ContactProBlazor.Interfaces;
+using ContactProBlazor.Client.Models;
+using ContactProBlazor.Client.Interfaces;
+
+namespace ContactProBlazor.Services
+{
+    public class ContactDTOService(IContactRepository repository) : IContactDTOService
+    {
+        public Task<ContactDTO> CreateContactAsync(ContactDTO contact, string userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
