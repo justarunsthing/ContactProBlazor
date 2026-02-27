9b944d80f240355d7380391195eab4d8ceec0415
justarunsthing <justarunsthing@outlook.com>
2026-02-27 08:02:56 +0000

    Added skeleton SendGridService class

---
+++ ContactProBlazor/Services/SendGridService.cs
@@ -0,0 +1,32 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using ContactProBlazor.Data;
+using Microsoft.AspNetCore.Identity;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace ContactProBlazor.Services
+{
+    public class SendGridService : IEmailSender, IEmailSender<ApplicationUser>
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendPasswordResetCodeAsync(ApplicationUser user, string email, string resetCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendPasswordResetLinkAsync(ApplicationUser user, string email, string resetLink)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendConfirmationLinkAsync(ApplicationUser user, string email, string confirmationLink)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
