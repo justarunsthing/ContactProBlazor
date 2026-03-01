7f9c7f5714a2d0fe2200aeeb31923985ad6b37b1
justarunsthing <justarunsthing@outlook.com>
2026-03-01 12:03:31 +0000

    Added skeleton ContactsController

---
+++ ContactProBlazor/Controllers/ContactsController.cs
@@ -0,0 +1,19 @@
+﻿using ContactProBlazor.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+using ContactProBlazor.Client.Interfaces;
+
+namespace ContactProBlazor.Controllers
+{
+    // api/contacts
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ContactsController(IContactDTOService contactDTOService, UserManager<ApplicationUser> userManager) : ControllerBase
+    {
+        // Authorize means userId will never be null
+        // Is an expression bodied member as it needs to be evaluated everytime this controller is accessed
+        private string _userId => userManager.GetUserId(User)!;
+    }
+}
