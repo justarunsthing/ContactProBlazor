e228fa2b87177b027566590b668c18119b43a8cf
justarunsthing <justarunsthing@outlook.com>
2026-03-02 07:59:12 +0000

    Register service

--- ContactProBlazor.Client/Program.cs
+++ ContactProBlazor.Client/Program.cs
@@ -11,5 +11,6 @@ builder.Services.AddAuthenticationStateDeserialization();
 // Services
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<ICategoryDTOService, WASMCategoryDTOService>();
+builder.Services.AddScoped<IContactDTOService, WASMContactDTOService>();
 
 await builder.Build().RunAsync();
