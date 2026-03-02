1508833acdffe99552cc1ef1b47177584b591ee8
justarunsthing <justarunsthing@outlook.com>
2026-03-02 07:45:57 +0000

    Added GetContacts endpoint

--- ContactProBlazor/Controllers/ContactsController.cs
+++ ContactProBlazor/Controllers/ContactsController.cs
@@ -1,6 +1,7 @@
 ﻿using ContactProBlazor.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
+using ContactProBlazor.Client.Models;
 using Microsoft.AspNetCore.Authorization;
 using ContactProBlazor.Client.Interfaces;
 
@@ -15,5 +16,27 @@ namespace ContactProBlazor.Controllers
         // Authorize means userId will never be null
         // Is an expression bodied member as it needs to be evaluated everytime this controller is accessed
         private string _userId => userManager.GetUserId(User)!;
+
+        [HttpGet]
+        // [HttpGet("{id}")]
+        public async Task<ActionResult<List<ContactDTO>>> GetContacts([FromQuery] int? categoryId)
+        {
+            try
+            {
+                if (categoryId != null)
+                {
+                    return await contactDTOService.GetContactsByCategoryAsync(categoryId.Value, _userId);
+                }
+                else
+                {
+                    return await contactDTOService.GetContactsAsync(_userId);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return Problem();
+            }
+        }
     }
 }
