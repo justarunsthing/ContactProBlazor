4639d7f74c080b23110d8e5165706483cdfbe768
justarunsthing <justarunsthing@outlook.com>
2026-03-07 09:17:02 +0000

    Implemented EmailCategoryAsync method

--- ContactProBlazor.Client/Services/WASMCategoryDTOService.cs
+++ ContactProBlazor.Client/Services/WASMCategoryDTOService.cs
@@ -46,9 +46,19 @@ namespace ContactProBlazor.Client.Services
             response.EnsureSuccessStatusCode();
         }
 
-        public Task<bool> EmailCategoryAsync(int id, EmailData emailData, string userId)
+        public async Task<bool> EmailCategoryAsync(int id, EmailData emailData, string userId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                HttpResponseMessage response = await http.PostAsJsonAsync($"api/categories/email/{id}", emailData);
+
+                return response.IsSuccessStatusCode;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return false;
+            }
         }
     }
 }
